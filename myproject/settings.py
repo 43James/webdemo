6e7b6d331a80363ab6ca4d46dc98e80420666f66
43James <222surasakm3@gmail.com>
2023-09-25 13:23:01 +0700

    webdemo2

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
 
 INSTALLED_APPS = [
     
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
